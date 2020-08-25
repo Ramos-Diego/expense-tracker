99f1b40148dc2807a6d57cd7ea5ace2bf340b09f
Ramos-Diego <RamosDiego@pm.me>
2020-08-25 07:44:56 -0400

    Updates

---
+++ precache-manifest.abd1e8ad2f664b39a442a61134d2fb37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37848885df2369ee3db5b5d88535537e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63abb2d575c3c12ff801",
+    "url": "./static/js/2.bec5b068.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "./static/js/2.bec5b068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36425291dd4b2dd23c93",
+    "url": "./static/js/main.82c71e84.chunk.js"
+  },
+  {
+    "revision": "46f00c63568a0040936a",
+    "url": "./static/js/runtime-main.bf6b2eda.js"
+  }
+]);
