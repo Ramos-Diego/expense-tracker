2a9a106234bdff828f6eca7ad9d3e70fc6316d2d
Diego Ramos <RamosDiego@pm.me>
2020-08-24 22:46:25 -0400

    Updates

---
+++ precache-manifest.b7f6ea4a7952515bd29765abc2eb017d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62c2168275fcea300356849a2ede221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302bfc65af2ef42ffde2",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "774c80d43508b2ab858d",
+    "url": "/static/js/2.bccc079a.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.bccc079a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302bfc65af2ef42ffde2",
+    "url": "/static/js/main.2092dbb7.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
